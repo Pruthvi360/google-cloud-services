97ea34128471f903b91463d3ca0f2bef0cc701db
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-12-07 15:40:31 +0530

    Create bigtable.tf

---
+++ bigtable/bigtable.tf
@@ -0,0 +1,37 @@
+resource "google_bigtable_instance" "production-instance" {
+  name = "tf-bigtable-instance"
+
+  # A cluster with fixed number of nodes.
+  cluster {
+    cluster_id   = "tf-instance-cluster1"
+    num_nodes    = 1
+    storage_type = "HDD"
+    zone    = "us-central1-c"
+  }
+
+  # a cluster with auto scaling.
+  cluster {
+    cluster_id   = "tf-instance-cluster2"
+    storage_type = "HDD"
+    zone    = "us-central1-b"
+    autoscaling_config {
+      min_nodes = 1
+      max_nodes = 3
+      cpu_target = 50
+    }
+  }
+
+  labels = {
+    my-label = "prod-label"
+  }
+}
+
+resource "google_bigtable_table" "table" {
+  name          = "tf-table"
+  instance_name = google_bigtable_instance.production-instance.name
+  split_keys    = ["a", "b", "c"]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
