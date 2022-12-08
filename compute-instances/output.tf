2dc044226b07038bb3bf2b227ebaa6c716168e7b
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-12-09 01:54:28 +0530

    Update output.tf

--- compute-instances/output.tf
+++ compute-instances/output.tf
@@ -1,5 +1,3 @@
 output "webserver_ip" {
   value = google_compute_instance.ansible-controller.network_interface.0.access_config.0.nat_ip
-  value = google_compute_instance.ansible-local-host-1.network_interface.0.access_config.0.nat_ip
-  value = google_compute_instance.ansible-local-host-2.network_interface.0.access_config.0.nat_ip
 }
