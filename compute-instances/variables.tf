2ed5811ee6db4aa28e68bcc4e1056091292ac565
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-12-09 01:16:35 +0530

    Update variables.tf

--- compute-instances/variables.tf
+++ compute-instances/variables.tf
@@ -2,3 +2,32 @@ variable "project_id" {
 type =string
 default = "ansible-demo-371016"
 }
+
+variable "region" {
+  description = "GCP region name."
+  type        = string
+  default     = "us-west4"
+}
+
+variable "zone" {
+  description = "GCP zone name."
+  type        = string
+  default     = "us-west4-b"
+}
+
+variable "name" {
+  description = "Web server name."
+  type        = string
+  default     = "my-webserver"
+}
+
+variable "machine_type" {
+  description = "GCP VM instance machine type."
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "labels" {
+  description = "List of labels to attach to the VM instance."
+  type        = ansible
+}
