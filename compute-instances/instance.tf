f76322eff34368118a733ed860f5c09a0d749dd5
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-12-08 23:13:23 +0530

    Update instance.tf

--- compute-instances/instance.tf
+++ compute-instances/instance.tf
@@ -1,10 +1,29 @@
+# Create a single Compute Engine instance
+
 resource "google_compute_instance" "ansible-controller" {
-  name = "ansible-controller"
-  project = "var.project_id"
-  zone = "us-central1-a"
+  name         = "ansible-controller"
+  project      = "var.project_id"
+  zone         = "us-central1-a"
   machine_type = "e2-micro"
-  provisioning_model = "SPOT"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Ansible
   metadata_startup_script = "sudo apt-get update; sudo apt-get install ansible"
+  provisioning_model = "SPOT"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
 }
 
 resource "google_compute_instance" "ansible-local-host-1" {
@@ -12,8 +31,25 @@ resource "google_compute_instance" "ansible-local-host-1" {
   project = "var.project_id"
   zone = "us-central1-a"
   machine_type = "e2-micro"
-  provisioning_model = "SPOT"
   lables = "ansible-host"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+ 
+  provisioning_model = "SPOT"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
 }
 
 resource "google_compute_instance" "ansible-local-host-2" {
@@ -21,6 +57,23 @@ resource "google_compute_instance" "ansible-local-host-2" {
   project = "var.project_id"
   zone = "us-central1-a"
   machine_type = "e2-micro"
-  provisioning_model = "SPOT"
   lables = "ansible-host"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+ 
+  provisioning_model = "SPOT"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
 }
