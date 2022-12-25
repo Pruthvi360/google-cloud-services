c4cbeefce990b120a681d57c89716f3e542c4c53
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2022-12-25 09:58:38 +0530

    Update install-kubernetes.sh

--- gke-deployment/install-kubernetes.sh
+++ gke-deployment/install-kubernetes.sh
@@ -6,6 +6,7 @@ sudo systemctl enable docker
 sudo apt-get install curl
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 777 /etc/apt/sources.list.d/kubernetes.list
 sudo swapoff -a
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
