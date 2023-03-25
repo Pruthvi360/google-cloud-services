71b752ecbf2bbcdb302a5fd621a7ebe2145f4376
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-03-25 18:27:23 +0530

    Create setup-script-compute-vm.sh

---
+++ cloud-profiler/python/setup-script-compute-vm.sh
@@ -0,0 +1,11 @@
+##Install the C/C++ compiler and development tools:
+
+sudo apt-get install -y build-essential
+
+##Install pip:
+
+sudo apt-get install -y python3-pip
+
+##Install the Profiler package:
+
+pip3 install google-cloud-profiler
