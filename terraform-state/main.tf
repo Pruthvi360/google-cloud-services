c08bfa7fd88c76b1e3f0b7910713c2f4cd7743ad
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-03-27 12:38:44 +0530

    Create main.tf

---
+++ terraform-state/main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project     = "on-prem-381812"
+  region      = "us-central-1"
+}
+resource "google_storage_bucket" "test-bucket-for-state" {
+  name        = "terraform-state-1996"
+  location    = "US"
+  uniform_bucket_level_access = true
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "terraform-state-1996"
+    prefix  = "terraform/state"
+  }
+}
