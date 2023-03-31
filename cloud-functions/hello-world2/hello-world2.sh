40df7c71b39a38c2db669830eb5225b558856ba8
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-03-31 18:05:42 +0530

    Update hello-world2.sh

--- cloud-functions/hello-world2/hello-world2.sh
+++ cloud-functions/hello-world2/hello-world2.sh
@@ -2,9 +2,7 @@
 
 PROJECT_ID=$(gcloud config get-value project)
 REGION=us-west1
-PROJECT_NUM=$(gcloud projects list \
---filter="$(gcloud config get-value project)" \
---format="value(PROJECT_NUMBER)")
+PROJECT_NUM=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
 
 # enable services API
 
